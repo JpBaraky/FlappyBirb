d768a0249862b21c68d3b83ed1f38661646ec072
João Pedro Baraky <jp.baraky@gmail.com>
2021-11-20 14:34:38 -0300

    Simple Background move
    -Now the background moves as the birb fly

---
+++ Assets/Scripts/moveOffSet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOffSet : MonoBehaviour
+{
+    private Material material;
+    private playerScript playerScript;
+    [Header("Movement Systems")]
+    public float offSetSpeed;
+    private float offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //## Component to variable starters ##
+        playerScript = GetComponent<playerScript>();
+        material = GetComponent<Renderer>().material;
+        //##
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offSet += offSetSpeed * Time.deltaTime; 
+        material.SetTextureOffset("_MainTex" , new Vector2(offSet, 0));   // Increase the offset of the object in a certain rate 
+       if(offSet >= 1){
+           offSet = 0;
+       }
+        
+    }
+}
